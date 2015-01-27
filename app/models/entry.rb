a19168794526c42d4c74362304c10889c51052f7
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-27 11:13:11 -0500

    Add various methods for formatting times

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,38 +1,84 @@
 class Entry < ActiveRecord::Base
 
-  def calc_duration(start, stop)
-    start - stop
+  def calculate_duration
+    self.start - self.stop
   end
 
   def time
     Time.now
   end
 
-  def date_from_now(time)
-    #"#{t.year}-#{t.month}-#{t.day}"
-    "#{t.month}-#{t.day}-#{t.year}"
+  def to_time(time_string)
+    if time_string.class == "Time"
+      return time_string
+    elsif time_string.class == "String"
+      hour = time_string[0..1].sub(":", "")
+      minute = time_string[3..4]
+      day, month, year = Time.now.day, Time.now.month, Time.now.year
+      return Time.new(year, month, day, hour, minute)
+    end
   end
 
   def tomorrow(time)
     time + 24 * 60 * 60
   end
 
-  def day_formatted
-    self.start.start.to_date
+  def date_today
+    day = Time.now.day
+    year = Time.now.year
+    month = Time.now.year
+    {:day => day, :year => year, :month => month}
+  end
+
+  def second_now
+    Time.now.sec
   end
 
   def start_time_formatted
-    time_formatted(self.start)
+    format_time(self.start)
   end
 
   def stop_time_formatted
-    time_formatted(self.stop)
+    format_time(self.stop)
   end
 
-  def time_formatted(time)
+  def convert_seconds_to_time_duration(seconds)
+    minutes = seconds.to_i / 60
+    hours = minutes / 60
+    minutes = minutes % 60
+    seconds = (seconds % 60).to_i
+    { :seconds => seconds, :minutes => minutes, :hours => hours }
+  end
+
+  def format_duration
+    duration = self.convert_seconds_to_time_duration(self.duration)
+    duration.keys.each do |unit|
+      if duration[unit].to_s.length == 1
+        duration[unit] = "0#{duration[unit].to_s}"
+      else
+        duration[unit] = duration[unit].to_s
+      end
+    end
+    "#{duration[:hours]}:#{duration[:minutes]}:#{duration[:seconds]}"
+  end
+
+  def format_date(time)
+    month_dict = {1 => "Jan", 2 => "Feb", 3 => "Mar", 4 => "Apr", 5 => "May", 6 => "Jun", 7 => "Jul", 8 => "Aug", 9 => "Sep", 10 => "Oct", 11 => "Nov", 12 => "Dec"}
+    day_dict = { 1 => "Mon", 2 =>"Tue", 3 =>"Wed", 4 =>"Thu", 5 =>"Fri", 6 =>"Sat", 7 => "Sun"}
+    day_of_week = day_dict[time.wday]
+    year = time.year
+    month = month_dict[time.month]
+    day = time.day
+    "#{day_of_week} #{month} #{day}, #{year}"
+  end
+
+#"Fri Jan 09, 2015"
+
+  def format_time(time)
     hour = time.hour
     minute = time.min
-    "#{hour}:#{minute}"
+    second = time.sec
+    "#{hour}:#{minute}:#{second}"
   end
 
 end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,56 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def test_calculate_duration_1
+    test "Calculates duration rounded to the nearest second" do
+      entry = Entry.new(:start => Time.now, :stop => (Time.now + 1))
+        assert entry.calculate_duration == 1.0
+    end
+  end
+
+  def test_format_time
+    test "Formats a time as a string in hh:mm:ss format" do
+      t = Time.new(2014, 12, 25, 22, 30, 15)
+      x = Entry.new
+      result_string = "22:30:15"
+        assert_equal x.time_formatted(t), result_string
+    end
+  end
+
+  def test_format_date
+    test "Extracts date from time and formats as day mmm dd, yyyy" do
+      d = Time.new(2014, 12, 25, 22, 30, 15)
+      result_string = "Thu Dec 25, 2014"
+      assert_equal x.format_date(d), result_string
+    end
+  end
+
+  def test_to_time
+    test "Converts a string of the form 'hh:mm' to a Time object" do
+      time_string = "10:30"
+      assert_equal to_time(time_string), Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 30)
+    end
+  end
+
+  def convert_seconds_to_time_duration_1
+    test "Converts a duration in number of seconds to the form 'hh:mm'" do
+      time1 = Time.new(2015, 1, 27, 10, 30)
+      time2 = Time.new(2015, 1, 27, 10, 31, 5)
+      Entry.new(:duration => time2 - time1)
+      time_string = "0:01:05"
+      assert_equal duration_format(time2 - time1), time_string
+    end
+  end
+
+  def convert_seconds_to_time_duration_2
+    test "Converts a duration in number of seconds to the form 'hh:mm'" do
+      time1 = Time.new(2015, 1, 27, 10, 30)
+      time2 = Time.new(2015, 1, 27, 11, 36, 5)
+      Entry.new(:duration => time2 - time1)
+      time_string = "01:06:05"
+      assert_equal duration_format(time2 - time1), time_string
+    end
+  end
+
 end

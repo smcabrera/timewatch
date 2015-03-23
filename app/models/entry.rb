d9b351ec97ae8fb08a2f5ed99fffbea13cc387a6
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-03-23 10:36:20 -0500

    Refactoring: cleaning up unnecessary code

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -41,6 +41,7 @@ class Entry < ActiveRecord::Base
   private
 
   def format_time(time)
+    # This line is a little strange--ActiveRecord saves times in a format that defaults to Greenwich Mean Time. to_time fixes this
     time ? time.to_time.strftime("%H:%M:%S") : "--"
   end
 
@@ -48,22 +49,8 @@ class Entry < ActiveRecord::Base
     month_dict = { 1 => "Jan", 2 => "Feb", 3 => "Mar", 4 => "Apr", 5 => "May", 6 => "Jun", 7 => "Jul", 8 => "Aug", 9 => "Sep", 10 => "Oct", 11 => "Nov", 12 => "Dec" }
     day_dict = { 1 => "Mon", 2 =>"Tue", 3 =>"Wed", 4 =>"Thu", 5 =>"Fri", 6 =>"Sat", 7 => "Sun" }
     day_of_week = day_dict[time.wday]
-    year = time.year
     month = month_dict[time.month]
-    day = time.day
-    "#{day_of_week} #{month} #{day}, #{year}"
+    "#{day_of_week} #{month} #{time.day}, #{time.year}"
   end
-
-  def to_time(time_string)
-    if time_string.class == Time
-      return time_string
-    elsif time_string.class == String
-      hour = time_string[0..1].sub(":", "")
-      minute = time_string[3..4]
-      day, month, year = Time.now.day, Time.now.month, Time.now.year
-      return Time.new(year, month, day, hour, minute)
-    end
-  end
-
 end
 
